a5541e385228aabac6475118d9ce540fb409e7ea
William <willspri@gmail.com>
2025-01-28 11:52:38 +1300

    tidied

--- script.js
+++ script.js
@@ -7,15 +7,14 @@ function setup() {
 }
 
 function draw() {
-    //noStroke();
-    //fill(255, 50);
-    //circle(mouseX, mouseY, 10);
-    noFill();
-    stroke(255, 50);
-    strokeWeight(2);
-    line(prevX, prevY, mouseX, mouseY);
-    prevX = mouseX;
-    prevY = mouseY;
+    if (mouseIsPressed) {
+        //noFill();
+        stroke(255, 50);
+        strokeWeight(2);
+        line(prevX, prevY, mouseX, mouseY);
+        prevX = mouseX;
+        prevY = mouseY;
+    }
 }
 
 function mousePressed() {
