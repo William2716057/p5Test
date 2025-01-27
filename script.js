cdd3dbf6af4f2f69e33a54f53a8c97036f2d490e
William <willspri@gmail.com>
2025-01-28 11:45:20 +1300

    updated

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+
+let prevX = 0;
+let prevY = 0;
 function setup() {
     createCanvas(800, 600);
     background(0);
 }
 
 function draw() {
-    noStroke();
-    fill(255, 50);
-    circle(mouseX, mouseY, 24);
+    //noStroke();
+    //fill(255, 50);
+    //circle(mouseX, mouseY, 10);
+    noFill();
+    stroke(255, 50);
+    strokeWeight(2);
+    line(prevX, prevY, mouseX, mouseY);
+    prevX = mouseX;
+    prevY = mouseY;
 }
 
 function mousePressed() {
